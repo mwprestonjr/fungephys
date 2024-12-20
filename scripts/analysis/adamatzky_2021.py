f38f041286126a9f4ff0329ca4d1440d61354cb0
Michael <mpreston@ucsd.edu>
2024-12-20 11:46:44 -0800

    add adamatsky 2021 analysis

---
+++ scripts/analysis/adamatzky_2021.py
@@ -0,0 +1,74 @@
+"""
+Analysis of Adamatzky 2021 data. These results primarily contribute to Figure 2.
+
+"""
+
+# imports
+import os
+import numpy as np
+import pandas as pd
+from neurodsp.spectral import compute_spectrum
+
+import sys
+sys.path.append("code")
+from analysis import compute_exponent, compute_complexity
+
+# settings
+DIR_INPUT = r"C:\Users\micha\datasets\adamatzky_2021\txt"
+
+# dataset details
+FS = 1 # Sampling frequency in Hz
+N_CHANNELS = 7 # Number of recording channels
+N_SPECIES = 4 # Number of species in the dataset
+
+
+def main():
+    # identify/create directories
+    path_out = "./data/adamatzky_2021"
+    for folder in ['psd', 'results']:
+        if not os.path.exists(f"{path_out}/{folder}"): 
+            os.makedirs(f"{path_out}/{folder}")
+
+    # loop through each species
+    exponent = np.zeros([N_SPECIES, N_CHANNELS])
+    complexity = np.zeros([N_SPECIES, N_CHANNELS])
+    for ii, fname in enumerate(os.listdir(DIR_INPUT)):
+        # load data
+        data_in = pd.read_csv(os.path.join(DIR_INPUT, fname), sep='\t')
+        signals = epoch_data(data_in, N_CHANNELS)
+
+        # compute spectra
+        freqs, spectra = compute_spectrum(signals, FS, nperseg=2**12)
+        fname_out = fname.replace('.txt', '.npz')
+        np.savez(f"{path_out}/psd/{fname_out}", spectra=spectra, freqs=freqs)
+
+        # compute spectral exponent and complexity
+        exponent[ii] = compute_exponent(spectra, freqs)
+        complexity[ii] = compute_complexity(signals)
+        np.save("data/adamatzky_2021/results/exponent.npy", exponent)
+        np.save("data/adamatzky_2021/results/complexity.npy", complexity)
+
+
+def epoch_data(data, n_channels):
+    """
+    Custom funtion for Adamatzky 2021. 
+    """
+    # determine number of samples to use for analysis
+    shortest_signal = 263959
+    potential_artifact = 50000
+    n_samples = shortest_signal - potential_artifact
+
+    # epoch data (trim to desired number of samples)
+    data = data[-n_samples:]
+
+    # interpolate NaN values
+    data = data.interpolate(method='linear', axis=0)
+
+    # grab desired channels
+    data = data.values[:, :n_channels].T
+
+    return data
+
+
+if __name__ == "__main__":
+    main()
