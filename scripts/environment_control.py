97a80258f5f6a58abeb8b744efdbfaf89dcf11a2
mpreston <mpreston@ucsd.edu>
2025-02-12 17:34:12 -0500

    add data collection

--- scripts/environment_control.py
+++ scripts/environment_control.py
@@ -18,6 +18,9 @@ from adafruit_sht31d import SHT31D
 import board
 import busio
 
+import numpy as np
+import pandas as pd 
+
 # Initialize I2C for SHT30
 i2c = busio.I2C(board.SCL, board.SDA)
 sht = SHT31D(i2c)
@@ -27,6 +30,8 @@ bus = smbus2.SMBus(1)
 arduino_address = 0x04
 
 # Control settings
+FILENAME = "data/environment/temp.csv"
+
 LIGHT_ON_TIME = 8  # Light ON time (24-hour format)
 LIGHT_OFF_TIME = 20  # Light OFF time (24-hour format)
 
@@ -46,23 +51,42 @@ def main():
     last_fan_time = time.time() - 3600  # Ensure fan runs on startup
     fan_status, fan_ran = False, False
 
-    while True:
-        # Read temperature and humidity
-        try:
-            humidity = sht.relative_humidity
-            temperature = sht.temperature
-            temperature_f = celcius_to_fahrenheit(temperature)
-            print(f"\nEnvironmental conditions:\n\tTemperature: {temperature:.2f}°C ({temperature_f:.2f}°F) \n\tHumidity: {humidity:.2f}%")
-        except Exception as e:
-            print(f"Failed to read from sensor: {e}")
-            continue
+    data_times = []
+    temperature_log = []
+    humidity_log = []
+    light_log = []
 
-        # Control devices
-        control_humidifier(humidity)
-        light_status = control_light(light_status)
-        last_fan_time, fan_status, fan_ran = control_fan(last_fan_time, fan_status, fan_ran)
-        
-        time.sleep(10)  # Delay between checks
+    try:
+        while True:
+            # Read temperature and humidity
+            try:
+                data_time = datetime.now()
+                humidity = sht.relative_humidity
+                temperature = sht.temperature
+                temperature_f = celcius_to_fahrenheit(temperature)
+                print(f"\nEnvironmental conditions:\n\tTemperature: {temperature:.2f}°C ({temperature_f:.2f}°F) \n\tHumidity: {humidity:.2f}%\n")
+            except Exception as e:
+                print(f"Failed to read from sensor: {e}")
+                continue
+
+            # collect data
+            data_times.append(data_time)
+            temperature_log.append(temperature)
+            humidity_log.append(humidity)
+            light_log.append(light_status)
+
+            # Control devices
+            control_humidifier(humidity)
+            light_status = control_light(light_status)
+            last_fan_time, fan_status, fan_ran = control_fan(last_fan_time, fan_status, fan_ran)
+            
+            time.sleep(10)  # Delay between checks
+
+    # if keyboard interrupt, save data
+    except KeyboardInterrupt:
+        save_data(data_time, temperature_log, humidity_log, light_log)
+        print("\n======= Stopping environment control script =======")
+        exit(0)
 
 
 def send_command(command):
@@ -120,6 +144,21 @@ def control_fan(last_fan_time, fan_status, fan_ran):
         print("Fan OFF")        
     
     return last_fan_time, fan_status, fan_ran
+
+
+def save_data(data_times, temperature_log, humidity_log, light_log):
+    print("\nSaving data...")
+
+    # save data to CSV
+    data = {
+        'Time': data_times,
+        'Temperature (°C)': temperature_log,
+        'Humidity (%)': humidity_log,
+        'Light Status': light_log
+    }
+    df = pd.DataFrame(data)
+    df.to_csv(FILENAME, index=False)
+    print(f"Data saved to {FILENAME}")
 
 
 def celcius_to_fahrenheit(celsius):
